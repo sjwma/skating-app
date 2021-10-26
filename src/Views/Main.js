9f651b517dbcebe52f795ea9dc9a874f5a3fd52b
sjwma <samuel.ma1024@gmail.com>
2021-10-26 17:38:18 -0500

    add additional libraries

---
+++ src/Views/Main.js
@@ -0,0 +1,14 @@
+import Button from '../Components/Button';
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="justify-items-center items-center content-center">
+            Skate
+            <Button text="Login" />
+            <Button text="Sign up" />
+        </div>
+    );
+};
+
+export default Main;
